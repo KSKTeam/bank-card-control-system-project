7c7cb68328c0407d8ad361fbf237fbf20a4fed62
JackKarichkovskiy <karichkovsky@yandex.com>
2015-12-20 23:10:45 +0200

    restored previous tests

--- src/test/java/jpa/hibernate/HibernateServiceTest.java
+++ src/test/java/jpa/hibernate/HibernateServiceTest.java
@@ -21,64 +21,64 @@ public class HibernateServiceTest {
 	
 	private Transaction transaction;
 
-//	@BeforeClass
-//	public static void setUpClass() {
-//		service = HibernateService.getInstance();
-//	}
-//
-//	@AfterClass
-//	public static void setDownClass() {
-//		if (service != null) {
-//			service.destroy();
-//		}
-//	}
-//
-//	@Before
-//	public void beforeTest(){
-//		transaction = createTransaction();
-//		
-//		if(transaction != null)
-//			service.insert(transaction);
-//	}
-//	
-//	@After
-//	public void afterTest(){
-//		if(transaction != null)
-//			service.delete(transaction);
-//	}
-//	
-//	@Test
-//	public void testGetInstance() {
-//		HibernateService service1 = HibernateService.getInstance();
-//		HibernateService service2 = HibernateService.getInstance();
-//		boolean result = service1 == service2;
-//		boolean expResult = true;
-//		assertEquals(expResult, result);
-//	}
-//
-//	@Test
-//	public void testRead() {
-//		Transaction result = service.read(transaction.getId(), Transaction.class);
-//		Transaction expResult = transaction;
-//		assertEquals(expResult, result);
-//	}
-//
-//	@Test
-//	public void testDelete() {
-//		service.delete(transaction);
-//		Transaction result = service.read(transaction.getId(), Transaction.class);
-//		assertNull(result);
-//		transaction = null;
-//	}
-//
-//	@Test
-//	public void testUpdate() {
-//		transaction.setDescription("Another test description");
-//		service.update(transaction);
-//		Transaction updatedTransaction = service.read(transaction.getId(), Transaction.class);
-//		Transaction expResult = transaction;
-//		assertEquals(expResult, updatedTransaction);
-//	}
+	@BeforeClass
+	public static void setUpClass() {
+		service = HibernateService.getInstance();
+	}
+
+	@AfterClass
+	public static void setDownClass() {
+		if (service != null) {
+			service.destroy();
+		}
+	}
+
+	@Before
+	public void beforeTest(){
+		transaction = createTransaction();
+		
+		if(transaction != null)
+			service.insert(transaction);
+	}
+	
+	@After
+	public void afterTest(){
+		if(transaction != null)
+			service.delete(transaction);
+	}
+	
+	@Test
+	public void testGetInstance() {
+		HibernateService service1 = HibernateService.getInstance();
+		HibernateService service2 = HibernateService.getInstance();
+		boolean result = service1 == service2;
+		boolean expResult = true;
+		assertEquals(expResult, result);
+	}
+
+	@Test
+	public void testRead() {
+		Transaction result = service.read(transaction.getId(), Transaction.class);
+		Transaction expResult = transaction;
+		assertEquals(expResult, result);
+	}
+
+	@Test
+	public void testDelete() {
+		service.delete(transaction);
+		Transaction result = service.read(transaction.getId(), Transaction.class);
+		assertNull(result);
+		transaction = null;
+	}
+
+	@Test
+	public void testUpdate() {
+		transaction.setDescription("Another test description");
+		service.update(transaction);
+		Transaction updatedTransaction = service.read(transaction.getId(), Transaction.class);
+		Transaction expResult = transaction;
+		assertEquals(expResult, updatedTransaction);
+	}
 
 	private Transaction createTransaction(){
 		Role role = new Role();
